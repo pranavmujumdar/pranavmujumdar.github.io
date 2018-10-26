7b71a190b5a89e05f479e5792f02c3adac9046c6
Pranav Mujumdar <psmujumdar@gmail.com>
2018-10-26 13:50:53 +0530

    minor changes in script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,104 +1,105 @@
-  // Type animation script
-  var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-  };
+// Type animation script
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
-  TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-    }
-
-    setTimeout(function() {
-    that.tick();
-    }, delta);
-  };
-
-  window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
-  };
-
-  //smooth scroll
-  var scrollLink = $('.scroll');
-
-  scrollLink.click(function(e){
-    e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 600)
-  })
-
-
-  var width=$(window).width();
-
-  function mobileNavbar(width){
-  if(width > 960){
-    $(window).scroll(parallax);
-    $(window).scroll(navbarScrolling);
-  } else if (width > 355 ){
-    $('.burgerdiv').css({
-      'margin-left': '5vw'
-    });
-    $('.site-title').css({
-      'margin-left': '8vw',
-      'margin-right': '9vw'
-    });
-    $('.contact-me').css({
-      'margin-left': '3vw',
-    });
-    $(window).scroll(navbarScrolling);
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
-      $('.burgerdiv').css({
-      'margin-left': '5vw'
-    });
-    $('.site-title').css({
-      'margin-left': '7vw',
-      'margin-right': '7vw'
-    });
-    $('.contact-me').css({
-      'margin-left': '3vw',
-    });
-    $(window).scroll(navbarScrolling);
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
+//smooth scroll
+var scrollLink = $('.scroll');
+
+scrollLink.click(function(e){
+  e.preventDefault();
+  $('body,html').animate({
+    scrollTop: $(this.hash).offset().top
+  }, 600)
+});
+
+
+var width=$(window).width();
+
+function mobileNavbar(width){
+if(width > 960){
+  $(window).scroll(parallax);
+  $(window).scroll(navbarScrolling);
+} else if (width > 355 ){
+  $('.burgerdiv').css({
+    'margin-left': '5vw'
+  });
+  $('.site-title').css({
+    'margin-left': '8vw',
+    'margin-right': '9vw'
+  });
+  $('.contact-me').css({
+    'margin-left': '3vw',
+  });
+  $(window).scroll(navbarScrolling);
+} else {
+    $('.burgerdiv').css({
+    'margin-left': '5vw'
+  });
+  $('.site-title').css({
+    'margin-left': '7vw',
+    'margin-right': '7vw'
+  });
+  $('.contact-me').css({
+    'margin-left': '3vw',
+  });
+  $(window).scroll(navbarScrolling);
+ }
 }
+
 $(function() {
   mobileNavbar($(this).width());
   $(window).resize(function() {
@@ -158,6 +159,13 @@ function parallax(){
 }
 
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
 
 
 
@@ -221,10 +229,3 @@ function parallax(){
 //     }  
 // }
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
